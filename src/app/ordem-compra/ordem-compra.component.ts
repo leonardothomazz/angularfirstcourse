d2d9c4c283cb0a1f57f09d70194b5912f2e61415
Leonardo Thomaz <leonardo.thomaz@sinqia.com.br>
2019-11-18 19:06:05 -0300

    novas atualizações

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -25,6 +25,9 @@ export class OrdemCompraComponent implements OnInit {
   public complementoEstadoPrimitivo: boolean = true;
   public formaPagamentoEstadoPrimitivo: boolean = true;
 
+  //controlar botão confirmar compra
+  public formEstado: string = 'disabled';
+
   constructor() { }
 
   ngOnInit() {
@@ -41,6 +44,8 @@ export class OrdemCompraComponent implements OnInit {
       this.enderecoValido = false;
     }
 
+    this.habilitaForm();
+
   }
 
   public atualizaNumero(numero: string): void {
@@ -53,6 +58,8 @@ export class OrdemCompraComponent implements OnInit {
     } else {
       this.numeroValido = false;
     }
+
+    this.habilitaForm();
 
   }
 
@@ -68,7 +75,8 @@ export class OrdemCompraComponent implements OnInit {
     } else {
       this.complementoValido = false;
     }
-
+    
+    this.habilitaForm();
   }
 
   public atualizaFormaPagamento(formaPagamento: string) : void {
@@ -82,6 +90,17 @@ export class OrdemCompraComponent implements OnInit {
       this.formaPagamentoValido = false;
     }
 
+    this.habilitaForm();
+
+  }
+
+  public habilitaForm(): void {
+    if (this.enderecoValido === true && this.numeroValido === true && this.formaPagamentoValido === true) {
+      this.formEstado = '';
+    } else {
+      this.formEstado = 'disabled';
+    }
+    
   }
 
 }
